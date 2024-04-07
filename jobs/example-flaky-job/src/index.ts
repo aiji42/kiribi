df8f9c9d9c51b716ad3b248fc74f51970958c9b7
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2024-04-07 21:39:48 +0900

    update example job

--- jobs/example-flaky-job/src/index.ts
+++ jobs/example-flaky-job/src/index.ts
@@ -1,6 +1,10 @@
 import { WorkerEntrypoint } from 'cloudflare:workers';
 
 export default class extends WorkerEntrypoint {
+	async fetch() {
+		return new Response('OK');
+	}
+
 	async execute(payload: { delay: number; chanceOfSuccess: number }) {
 		console.log('Executing job', payload);
 		const { delay, chanceOfSuccess } = payload;
