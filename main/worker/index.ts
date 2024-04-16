c2fe2c559ab9db25bba9467e63cda41893669070
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2024-04-16 18:09:56 +0900

    update example job

--- main/worker/index.ts
+++ main/worker/index.ts
@@ -9,13 +9,15 @@ export default class extends Kiribi {
 }
 
 // you can split this class into another worker
-export class ExampleFlakyJob extends KiribiJobWorker {
-	async perform(payload: { delay: number; chance: number }) {
-		console.log('Performing job', payload);
-		const { delay, chance } = payload;
+export class SlowJob extends KiribiJobWorker {
+	async perform(payload: number | string) {
+		await new Promise((r) => setTimeout(r, payload ? Number(payload) : 30000));
+	}
+}
 
-		await new Promise((resolve) => setTimeout(resolve, delay));
-
-		if (Math.random() > chance) throw new Error('Failed to perform job');
+export class FlakyJob extends KiribiJobWorker {
+	async perform(payload: number | string) {
+		const chance = payload ? Number(payload) : 0.5;
+		if (Math.random() > chance) throw new Error('Failed to perform job because of your daily behavior');
 	}
 }
