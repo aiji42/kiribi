afee489e2ec255293de8d3441dabaa3008367b7d
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2024-04-17 16:25:44 +0900

    update example for publish as demo site

--- example/src/index.ts
+++ example/src/index.ts
@@ -6,20 +6,37 @@ import { rest } from 'kiribi/rest';
 export default class extends Kiribi {
 	client = client;
 	rest = rest;
+	async scheduled() {
+		// delete jobs older than 10 minutes
+		await this.sweep({ createdAtLt: new Date(Date.now() - 1000 * 60 * 10), statusIn: '*' });
+	}
 }
 
 // performer workers; You can split these classes into another worker
 import { KiribiWorker } from 'kiribi/worker';
 
 export class SlowJob extends KiribiWorker {
-	async perform(payload: number | string) {
-		await new Promise((r) => setTimeout(r, payload ? Number(payload) : 30000));
+	async perform(payload: unknown) {
+		assertPayloadForSlowJob(payload);
+		await new Promise((r) => setTimeout(r, Math.max(Math.min(15000, payload.delay), 1000)));
 	}
 }
 
+function assertPayloadForSlowJob(payload: unknown): asserts payload is { delay: number } {
+	if (typeof payload !== 'object' || payload === null || !('delay' in payload))
+		throw new Error('Invalid payload; input `{ "delay": 1000~15000 }`');
+	if (typeof payload.delay !== 'number') throw new Error('Invalid payload; input `{ "delay": 1000~15000 }`');
+}
+
 export class FlakyJob extends KiribiWorker {
-	async perform(payload: number | string) {
-		const chance = payload ? Number(payload) : 0.5;
-		if (Math.random() > chance) throw new Error('Failed to perform job because of your daily behavior');
+	async perform(payload: unknown) {
+		assertPayloadForFlakyJob(payload);
+		if (Math.random() > Math.max(Math.min(1, payload.chance), 0)) throw new Error('Failed to perform job because of your daily behavior');
 	}
+}
+
+function assertPayloadForFlakyJob(payload: unknown): asserts payload is { chance: number } {
+	if (typeof payload !== 'object' || payload === null || !('chance' in payload))
+		throw new Error('Invalid payload; input `{ "chance": 0.0~1.0 }`');
+	if (typeof payload.chance !== 'number') throw new Error('Invalid payload; input `{ "chance": 0.0~1.0 }`');
 }
